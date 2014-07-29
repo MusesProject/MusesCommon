9302d93084239a3b49caa1dc69c8af6534d61d14
Christoph Stanik <christoph.stanik.developer@gmail.com>
2014-07-29 14:13:20 +0200

    added field for the Notification Sensor

--- src/main/java/eu/musesproject/client/model/contextmonitoring/InteractionObservedApps.java
+++ src/main/java/eu/musesproject/client/model/contextmonitoring/InteractionObservedApps.java
@@ -2,4 +2,5 @@ package eu.musesproject.client.model.contextmonitoring;
 
 public class InteractionObservedApps {
 	public static final String OBSERVED_GMAIL = "Gmail";
+	public static final String OBSERVED_GMAIL_PACKAGE = "com.google.android.gm";
 }
