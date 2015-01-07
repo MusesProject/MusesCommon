793b7a6865f633a83d0bda867477cac142e38386
szamarripa <szamarripa@s2grupo.es>
2015-01-07 15:34:35 +0100

    Adding missing common classes

--- src/main/java/eu/musesproject/client/model/RequestType.java
+++ src/main/java/eu/musesproject/client/model/RequestType.java
@@ -9,6 +9,7 @@ public class RequestType {
     public static final String UPDATE_CONTEXT_EVENTS    = "update_context_events"; // send offline stored context events
     public static final String UPDATE_POLICIES          = "update_policies"; // policies are sent
     public static final String LOGIN	        		= "login"; // authentication response to the server
+    public static final String LOGOUT	        		= "logout";
     public static final String AUTH_RESPONSE	        = "auth-response"; // authentication response from the server
     public static final String USER_ACTION				= "user_behavior";
 }
