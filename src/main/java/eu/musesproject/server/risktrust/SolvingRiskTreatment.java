977abfa39951bf0bc50b453c58442c267a724be1
xaviertiti <xavier.titi@gmail.com>
2015-01-20 16:23:32 +0100

    -Adding a class for defining the solving risktreatment field of Decision object

---
+++ src/main/java/eu/musesproject/server/risktrust/SolvingRiskTreatment.java
@@ -0,0 +1,11 @@
+package eu.musesproject.server.risktrust;
+
+
+public class SolvingRiskTreatment {
+	
+    public static final int VIRUS_FOUND           = 1; // Detection of a virus, the user must run an antivirus
+    public static final int UNSECURE_NETWORK          = 2; // The user is connected to unsecure network, he has to switch to secure network
+   
+    
+
+}
