4fcece51cbec8e8e81799eea0534fa17d42a3b91
Christoph <christophstanik@gmail.com>
2015-03-26 16:33:09 +0100

    Create Zone.java
    used in the location sensor

---
+++ src/main/java/eu/musesproject/client/model/contextmonitoring/Zone.java
@@ -0,0 +1,43 @@
+package eu.musesproject.client.model.contextmonitoring;
+
+/**
+ * Created by christophstanik on 3/26/15.
+ */
+public class Zone {
+    private int zoneId;
+    private double latitude;
+    private double longitude;
+    private float radius;
+
+    public int getZoneId() {
+        return zoneId;
+    }
+
+    public void setZoneId(int zoneId) {
+        this.zoneId = zoneId;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+}
