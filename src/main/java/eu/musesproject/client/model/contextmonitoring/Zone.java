90e8cda2a50ac5cbd85e0d4b46e249035d129ff9
Christoph Stanik <christoph.stanik.developer@gmail.com>
2015-04-24 16:06:38 +0200

    update of the commons project

---
+++ src/main/java/eu/musesproject/client/model/contextmonitoring/Zone.java
@@ -0,0 +1,52 @@
+package eu.musesproject.client.model.contextmonitoring;
+
+/**
+ * Created by christophstanik on 3/26/15.
+ */
+public class Zone {
+    private String description;
+    private int zoneId;
+    private double latitude;
+    private double longitude;
+    private float radius;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getZoneId() {
+        return zoneId;
+    }
+
+    public void setZoneId(int zoneId) {
+        this.zoneId = zoneId;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+}
